fd5ba6b4909f78d50321d4f13cae1b3148fed0b4
PETE <windpipe@gmail.com>
2026-03-18 16:50:06 +0900

    Add Windows build script (build.bat)

---
+++ build.bat
@@ -0,0 +1,20 @@
+@echo off
+set GLSLC=C:\VulkanSDK\1.3.216.0\Bin\glslc.exe
+set SHADER_DIR=shaders
+
+echo === Compiling shaders ===
+%GLSLC% -fshader-stage=comp %SHADER_DIR%\particle.comp.glsl -o %SHADER_DIR%\particle.comp.spv || goto :error
+%GLSLC% -fshader-stage=vert %SHADER_DIR%\particle.vert.glsl -o %SHADER_DIR%\particle.vert.spv || goto :error
+%GLSLC% -fshader-stage=frag %SHADER_DIR%\particle.frag.glsl -o %SHADER_DIR%\particle.frag.spv || goto :error
+echo Shaders OK
+
+echo === Building Odin ===
+odin build . -out:somsee.exe -o:speed || goto :error
+echo Done: somsee.exe
+goto :end
+
+:error
+echo BUILD FAILED
+exit /b 1
+
+:end
